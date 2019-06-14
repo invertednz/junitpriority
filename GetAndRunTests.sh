3d03ca925bb7824b42c2c4a71a034c34defabf18
invertednz <jamesfarrier@gmail.com>
2019-06-14 15:52:36 -0700

    Scripts for running tests

---
+++ GetAndRunTests.sh
@@ -0,0 +1,133 @@
+#url
+#url="http://appsurify.dev.appsurify.com"
+url=$1
+#API Key
+#apiKey="MTpEbzhXQThOaW14bHVQTVdZZXNBTTVLT0xhZ00"
+apiKey=$2
+#Project
+#project="2"
+project=$3
+#Test Suite
+#testsuite="1"
+testsuite=$4
+#get commit
+commitId=$5
+finalTestNames=""
+
+delete_reports () {
+if [[ $reporttype == "directory"]] ; then rm '$report*.xml' ; fi
+if [[ $reporttype == "file" ]] ; then rm $report ; fi
+}
+
+execute_tests () {
+if [[ $deletereports == "true" ]] ; then delete_reports ; fi
+$startrun$1$endrun
+echo $startrun$1$endrun
+. ./PushResultsToAppsurify.sh #"$1" "$2" "$3" "$4" "$commitId" "$report"
+}
+
+get_tests () {
+finalTestNames=""
+json=`curl --header "token: $apiKey" "$url/api/external/prioritized-tests/?project_name=$project&priority=5&full_name=$fullname&test_suite_name=$testsuite&commit=$commitId" | sed 's/\"//g'`
+#echo $json
+prop="name"
+values=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' |tr "," "\n" | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop | sed 's/\[//g' | sed 's/\]//g' | sed 's/name://g' `
+if [[ $json != *"name"* ]] ; then echo "No test found" ; values="" ; fi
+return $values
+}
+
+#could do something smart where we check to see if these are medium and rerun to only rerun medium tests not high and medium
+rerun_tests_execute () {
+get_tests $1
+count=0
+
+for testName in $?; do count=$((count+1));  echo $count; if [ $(( $count % $maxtests )) -eq "0" ]; then count=0; finalTestNames=$finalTestNames`echo $testseparator$prefixtest$testName$postfixtest`; finalTestNames=`echo $finalTestNames | sed 's/,$//g'`; execute_tests $finalTestNames ; finalTestNames=""; elif [ count == 1 ];  then finalTestNames=$prefixtest$testName$postfixtest; else finalTestNames=$finalTestNames`echo $testseparator$prefixtest$testName$postfixtest`; fi;done
+finalTestNames=`echo $finalTestNames | sed 's/,$//g'` ; fi
+if [[ $finalTestNames != "" ]] ; then execute_tests $finalTestNames ; fi
+}
+
+rerun_tests () {
+if [[ $rerun == "true" ]]; then 
+    numruns=0;
+    while [ $numruns -lt $maxrerun ]
+    do
+        rerun_tests_execute
+        (( numruns++ ))
+    done
+; fi  
+}
+
+failfast_tests () {
+if [[ $failfast == "true" ]] ; then
+    rerun_tests $1
+    . ./GetResultsFromAppsurify.sh #"$1" "$2" "$3" "$4" "$commitId" "$report"
+; fi       
+}
+
+
+##try to be smarter and fail as soon as a group of tests has failed and be rerun, so add the reruns to here somehow?
+get_and_run_tests () {
+get_tests $1
+#then echo $values
+#if [[ $json != *"name"* ]] ; then echo "No test found" ; values="" ; fi
+#for testName in $values; do count=$((count+1));  echo $count; if [ $(( $count % $maxtests )) -eq "0" ]; then finalTestNames=$finalTestNames`echo $starttests$testName,`; finalTestNames=`echo $finalTestNames | sed 's/,$//g'`; mvn -Dtest=$finalTestNames test ; finalTestNames=""; else finalTestNames=$finalTestNames`echo $starttests$testName,`; fi;done
+#finalTestNames=`echo $finalTestNames | sed 's/,$//g'` ; fi
+#if [[ $finalTestNames != "" ]] ; then mvn -Dtest=$finalTestNames test ; fi
+
+count=0
+
+#for testName in $?; do count=$((count+1));  echo $count; if [ $(( $count % $maxtests )) -eq "0" ]; then count=0; finalTestNames=$finalTestNames`echo $testseparator$prefixtest$testName$postfixtest`; finalTestNames=`echo $finalTestNames | sed 's/,$//g'`; execute_tests $finalTestNames ; failfast_tests ; finalTestNames=""; elif [ count == 1 ];  then finalTestNames=$prefixtest$testName$postfixtest; else finalTestNames=$finalTestNames`echo $testseparator$prefixtest$testName$postfixtest`; fi;done
+#finalTestNames=`echo $finalTestNames | sed 's/,$//g'` ; fi
+#if [[ $finalTestNames != "" ]] ; then execute_tests $finalTestNames ; failfast_tests ; fi
+execute_tests "sample.junit.PriorityTest.Test1, sample.junit.PriorityTest.Test2"
+}
+
+
+
+#mvn test
+
+###
+#if tests == all just run tests
+if [[ $teststorun == "all" ]] ; then execute_tests "" 0 ; fi
+
+index=0
+if [[ $teststorun == *high* ]] ; then testtypes[index]=1 ; ((index++)) ; fi
+if [[ $teststorun == *medium* ]] ; then testtypes[index]=2 ; ((index++)) ; fi
+if [[ $teststorun == *low* ]] ; then testtypes[index]=3 ; ((index++)) ; fi
+if [[ $teststorun == *unassigned* ]] ; then testtypes[index]=4 ; ((index++)) ; fi
+#if [[ $teststorun == *rerun* ]] ; then testtypes[index]=5 ; ((index++)) ; fi
+if [[ $teststorun == *open* ]] ; then testtypes[index]=6 ; ((index++)) ; fi
+if [[ $teststorun == *ready* ]] ; then testtypes[index]=7 ; ((index++)) ; fi
+
+
+####start loop
+for i in "${testtypes[@]}"
+do
+   echo "$i"
+   get_and_run_tests $i
+   
+   
+
+   # or do whatever with individual element of the array
+done
+
+if [[ $failfast == "false" && $rerun == "true" ]] ; then rerun_tests ; fi
+
+. ./GetResultsFromAppsurify.sh #"$1" "$2" "$3" "$4" "$commitId" "$report"
+#if not then get tests
+
+#run tests
+
+#push results
+
+#if fail fast rerun tests
+
+#check results and fail if fail fast
+
+#####end loop
+
+#check results
+
+#run_id="this is the runid"
+
+
---
+++ RunTestsWithAppsurify.sh
@@ -0,0 +1,156 @@
+
+fail="newdefects, reopeneddefects" #default new defects and reopened defects  #options newdefects, reopeneddefects, flakybrokentests, newflaky, reopenedflaky, failedtests, brokentests
+additionalargs="" #default ''
+endrun="" #default ''
+testseparator="" #default ' '
+postfixtest="" #default ''
+prefixtest="" #default ''
+fullnameseparator=" " #default ' '
+fullname="false" #default false
+failfast="false" #defult false
+maxrerun=3 #default 3
+rerun="false" #default false
+importtype="junit" #default junit
+reporttype="directory" #default directory
+teststorun="all" #options include - high, medium, low, unassigned, ready, open
+deletereports="true" #options true or false, BE CAREFUL THIS WILL DELETE THE SPECIFIC FILE OR ALL XML FILES IN THE DIRECTORY
+#startrun needs to end with a space sometimes
+#endrun needs to start with a space sometimes
+
+###############
+#atm rerunning tests and fast fail don't work together very well
+##########
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -u | --url )           shift
+                               url=$1
+                               ;;
+        -a | --apikey )        shift
+                               apiKey=$1
+                               ;;
+        -p | --project )       shift
+                               project=$1
+                               ;;
+        -ts | --testsuite )    shift
+                               testsuite=$1
+                               ;;
+        -r | --report )        shift
+                               report=$1
+                               ;;
+        -rt | --reporttype )   shift
+                               reporttype=$1
+                               ;;
+        -t | --teststorun )    shift
+                               teststorun=$1
+                               ;;
+        -i | --importtype )    shift
+                               importtype=$1
+                               ;;
+        -re | --rerun )        shift
+                               rerun=$1
+                               ;;
+        -mr | --maxrerun )     shift
+                               maxrerun=$1
+                               ;;
+        -ff | --failfast )     shift
+                               failfast=$1
+                               ;;
+        -fn | --fullname )     shift
+                               fullname=$1
+                               ;;
+        -fs | --fullnameseparator )         shift
+                                            fullnameseparator=$1
+                                            ;;
+        -sr | --startrun )     shift
+                               startrun=$1
+                               ;;
+        -pr | --prefixtest )   shift
+                               prefixtest=$1
+                               ;;
+        -po | --postfixtest )  shift
+                               postfixtest=$1
+                               ;;
+        -se | --testseparator )         shift
+                                        testseparator=$1
+                                        ;;
+        -er | --endrun )       shift
+                               endrun=$1
+                               ;;
+        -aa | --additionalargs )    shift
+                                    additionalargs=$1
+                                    ;;
+        -f | --fail )          shift
+                               fail=$1
+                               ;;
+        -d | --deletereports ) shift
+                               deletereports=$1
+                               ;;
+        -h | --help )          echo "please see url for more details on this script and how to execute your tests with appsurify"
+                               exit 1
+                               ;;
+    esac
+    shift
+done
+
+#echo $url
+#echo $apiKey
+#echo $project
+#echo $testsuite
+#echo $fail #default new defects and reopened defects
+#echo $additionalargs #default ''
+#echo $endrun #default ''
+#echo $testseparator #default ' '
+#echo $postfixtest #default ''
+#echo $prefixtest #default ''
+#echo $startrun
+#echo $fullnameseparator #default ' '
+#echo $fullname #default false
+#echo $failfast #defult false
+#echo $maxrerun #default 3
+#echo $rerun #default false
+#echo $importtype #default junit
+#echo $teststorun
+#echo $reporttype #default directory
+#echo $report #needs to end with a \ for directory or .xml for file
+
+if [[ $url == "" ]] ; then echo "no url specified" ; exit 1 ; fi
+if [[ $apiKey == "" ]] ; then echo "no apiKey specified" ; exit 1 ; fi
+if [[ $project == "" ]] ; then echo "no project specified" ; exit 1 ; fi
+if [[ $testsuite == "" ]] ; then echo "no testsuite specified" ; exit 1 ; fi
+if [[ $report == "" ]] ; then echo "no report specified" ; exit 1 ; fi
+if [[ $teststorun == "" ]] ; then echo "no teststorun specified" ; exit 1 ; fi
+if [[ $startrun == "" ]] ; then echo "no command used to start running tests specified" ; exit 1 ; fi
+
+####example RunTestsWithAppsurify.sh --url "http://appsurify.dev.appsurify.com" --apikey "MTpEbzhXQThOaW14bHVQTVdZZXNBTTVLT0xhZ00" --project "Test" --testsuite "Test" --report "report" --teststorun "all" --startrun "mvn -tests" --testsuites ""
+#example RunTestsWithAppsurify.sh --url "http://appsurify.dev.appsurify.com" --apikey "MTpEbzhXQThOaW14bHVQTVdZZXNBTTVLT0xhZ00" --project "Test" --testsuite "Test" --report "report" --teststorun "all" --startrun "C:\apache\apache-maven-3.5.0\bin\mvn tests " --testsuites ""
+
+#url
+#url="http://appsurify.dev.appsurify.com"
+##url=$1
+#API Key
+#apiKey="MTpEbzhXQThOaW14bHVQTVdZZXNBTTVLT0xhZ00"
+##apiKey=$2
+#Project
+#project="2"
+##project=$3
+#Test Suite
+#testsuite="1"
+##testsuite=$4
+#get commit
+commitId=`git log -1 --pretty="%H"`
+run_id=""
+##report=$5
+
+echo $commitId
+
+#$url $apiKey $project $testsuite $fail $additionalargs $endrun $testseparator $postfixtest $prefixtest $startrun $fullnameseparator $fullname $failfast $maxrerun $rerun $importtype $teststorun $reporttype $report $commitId $run_id
+echo "Getting tests to run"
+. ./GetAndRunTests.sh #"$1" "$2" "$3" "$4" "$commitId" 
+
+#. ./PushResultsToAppsurify.sh "$1" "$2" "$3" "$4" "$commitId" "$report"
+
+#echo $run_id
+
+#. ./GetResultsFromAppsurify.sh #"$1" "$2" "$3" "$4" "$commitId" "$report"
